89f4817e475fe4bcf694706563988d81416e7329
Mohammad Rubaith <rubaith@algosolver.com>
2021-06-02 14:33:11 +0600

    Material ui core is replaced with react-icons

--- src/Pages/Homepage/SubCampaignSection/CustomSliderNext.js
+++ src/Pages/Homepage/SubCampaignSection/CustomSliderNext.js
@@ -1,15 +1,14 @@
 import React from "react"
-import NavigateNextIcon from "@material-ui/icons/NavigateNext";
-import "./style.css"
+import { AiOutlineRight } from "react-icons/ai";
 
 const CustomSliderNext=(props)=> {
   const { onClick } = props;
   return (
     <div
       onClick={onClick}
-      className="next  absolute md:h-12 md:w-12 lg:h-14 lg:w-14 rounded-lg  grid place-items-center cursor-pointer bg-white hover:bg-secondary group duration-300"
+      className="next  absolute h-8 w-8 md:h-12 md:w-12 lg:h-14 lg:w-14 rounded-lg  grid place-items-center cursor-pointer bg-white hover:bg-secondary group duration-300"
     >
-      <NavigateNextIcon style={{ fontSize: "2rem" }} className="group-hover:text-white text-secondary"/>
+      <AiOutlineRight className="group-hover:text-white text-secondary" />
     </div>
   );
 }
--- src/Pages/Homepage/SubCampaignSection/CustomSliderPrev.js
+++ src/Pages/Homepage/SubCampaignSection/CustomSliderPrev.js
@@ -1,17 +1,13 @@
 import React from "react";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { AiOutlineLeft } from "react-icons/ai";
 
-function CustomSliderPrev({onClick}) {
- 
+function CustomSliderPrev({ onClick }) {
   return (
     <div
       onClick={onClick}
-      className="prev  absolute mr-3 md:h-12 md:w-12 lg:h-14 lg:w-14 rounded-lg  grid place-items-center cursor-pointer bg-white hover:bg-secondary group duration-300"
+      className="prev  absolute mr-3 h-8 w-8 md:h-12 md:w-12 lg:h-14 lg:w-14 rounded-lg  grid place-items-center cursor-pointer bg-white hover:bg-secondary group duration-300"
     >
-      <ChevronLeftIcon
-        style={{ fontSize: "2rem" }}
-        className="group-hover:text-white text-secondary"
-      />
+      <AiOutlineLeft className="group-hover:text-white text-secondary" />
     </div>
   );
 }
